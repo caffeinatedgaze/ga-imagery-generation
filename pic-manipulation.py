cafa6ff9b848ebf0d4b36c6748ebca445e3416a8
fenchelfen <lyamets.misha@gmail.com>
2019-03-23 02:15:24 +0300

    Iterate over all pixels

--- pic-manipulation.py
+++ pic-manipulation.py
@@ -1,9 +1,13 @@
 from PIL import Image, ImageFilter
 
 im = Image.open('/strg/ga-imagery-generation/default.png')
-im.show()
+# im.show()
 
 im_sharp = im.filter(ImageFilter.SHARPEN)
 # im_sharp.show()
 
-r, g, b = im_sharp.split()
+width, height = im.size
+for x in range(width):
+    for y in range(height):
+        r, g, b = im.getpixel((x, y))
+        print(r, g, b)
