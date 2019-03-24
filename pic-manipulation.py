2863847c615f13b5c548c162439da8a740a02384
fenchelfen <lyamets.misha@gmail.com>
2019-03-24 23:26:11 +0300

    Test euclide

--- pic-manipulation.py
+++ pic-manipulation.py
@@ -1,29 +1,40 @@
 from PIL import Image
+import os
 
-im_before = Image.open('/strg/ga-imagery-generation/src/default.jpg')
-im_after  = Image.open('/strg/ga-imagery-generation/src/posterize_13.jpg')
+original = Image.open('/strg/ga-imagery-generation/src/default.jpg')
 
 
 def compute_distance(im1, im2):
+    if im1.size != im2.size:
+        raise Exception('Images should be of equal size\n{} and {} are not'.
+                        format(im1.filename, im2.filename))
 
-    width, height = im_before.size
+    width, height = im1.size
     distance = 0
 
     for x in range(width):
         for y in range(height):
             r1, g1, b1 = im1.getpixel((x, y))
             r2, g2, b2 = im2.getpixel((x, y))
-            distance += ((r1 + g1 + b1) - (r2 + g2 + b2))**2
+            distance += ((r1 + g1 + b1) - (r2 + g2 + b2)) ** 2
     return distance
 
 
-print(compute_distance(im_before, im_after))
+directory = '/strg/ga-imagery-generation/src/'
+results = []
 
-# 5228711
-# 1054896           -> max green hue
-# 8959576           -> max saturation
-# 11565293          -> posterize 13
-# 285206550
-# 573377762
-# 1925280624
-# 4772632775        -> max brightness
+for pic in os.listdir(directory):
+    if pic.endswith('.jpg'):
+        image = Image.open(directory + pic)
+        results.append((pic, compute_distance(image, original)))
+
+for item in sorted(results, key=lambda x: x[1]):
+    print('{0:32}{1}'.format(item[0], item[1]))
+
+# default.jpg                     0
+# green_hue_max.jpg               1054896
+# small_dot.jpg                   5625244
+# posterize_13.jpg                11565293
+# lil_curve.jpg                   366959592
+# max_contrast.jpg                2034139623
+# contrast_min.jpg                3138894074
