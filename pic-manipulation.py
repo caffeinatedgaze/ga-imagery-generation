9e6f78ffc2780a243733f7faa2aa40e854e444d2
fenchelfen <lyamets.misha@gmail.com>
2019-03-24 22:31:17 +0300

    Demo euclide distance

--- pic-manipulation.py
+++ pic-manipulation.py
@@ -1,13 +1,29 @@
-from PIL import Image, ImageFilter
+from PIL import Image
 
-im = Image.open('/strg/ga-imagery-generation/default.png')
-# im.show()
+im_before = Image.open('/strg/ga-imagery-generation/src/default.jpg')
+im_after  = Image.open('/strg/ga-imagery-generation/src/posterize_13.jpg')
 
-im_sharp = im.filter(ImageFilter.SHARPEN)
-# im_sharp.show()
 
-width, height = im.size
-for x in range(width):
-    for y in range(height):
-        r, g, b = im.getpixel((x, y))
-        print(r, g, b)
+def compute_distance(im1, im2):
+
+    width, height = im_before.size
+    distance = 0
+
+    for x in range(width):
+        for y in range(height):
+            r1, g1, b1 = im1.getpixel((x, y))
+            r2, g2, b2 = im2.getpixel((x, y))
+            distance += ((r1 + g1 + b1) - (r2 + g2 + b2))**2
+    return distance
+
+
+print(compute_distance(im_before, im_after))
+
+# 5228711
+# 1054896           -> max green hue
+# 8959576           -> max saturation
+# 11565293          -> posterize 13
+# 285206550
+# 573377762
+# 1925280624
+# 4772632775        -> max brightness
