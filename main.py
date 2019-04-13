35401ac93a6c91ea603b367824ccbf4a89441935
fenchelfen <lyamets.misha@gmail.com>
2019-04-13 15:34:11 +0300

    Change path

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from genetic import *
 from PIL import Image
 import cfg
 
-input('Press any button')
-
 start = time()
 
 cfg.new_population = gen_population(cfg.pop_size, (cfg.x_bound, cfg.y_bound))
@@ -14,10 +12,13 @@ cfg.new_population = gen_population(cfg.pop_size, (cfg.x_bound, cfg.y_bound))
 if npy_list_sorted():
     cfg.new_population[0] = load('library/' + npy_list_sorted()[::-1][0])
 
+print('Loaded\t\t' , npy_list_sorted()[::-1][0])
+
 cfg.fitness = cal_pop_fitness(cfg.new_population, cfg.target)
 init_min_fitness = min(cfg.fitness)
 
-Image.fromarray(get_best()).save('tmp/' + 'strokes' + '.png', 'PNG')
+# Image.fromarray(get_best()[0]) # save('tmp/' + 'strokes' + '.png', 'PNG')
+
 
 chkpoint_fit = 0
 for i in range(2 ** 64):
@@ -32,16 +33,17 @@ for i in range(2 ** 64):
     cfg.new_population[0:parents.shape[0], :] = parents
     cfg.new_population[parents.shape[0]:, :] = cfg.offspring_mutation
 
-    print('%.6d - %.6f seconds' % (i, (time() - start)), end='\t\t')
-    print('Delta fitness -\t{}'.format(init_min_fitness - min(cfg.fitness)))
+    if i % 1000 == 0:
+        print('%.6d - %.6f seconds' % (i, (time() - start)), end='\t\t')
+        print('Delta fitness -\t{}'.format(init_min_fitness - min(cfg.fitness)))
 
     if i % 250 == 0:
-        save('library/_data__' + str(i), get_best())
-    if abs(chkpoint_fit - min(cfg.fitness)) >= 10000:
+        save('library/_data__' + str(cfg.fitness[get_best()[1]]), get_best()[0])
+    if abs(chkpoint_fit - min(cfg.fitness)) >= 100000:
         chkpoint_fit = min(cfg.fitness)
         # Image.fromarray(get_best()).save('tmp/' + 'strokes' + '.png', 'PNG')
-        Image.fromarray(get_best()).save('tmp/' + 'strokes' + '.jpg', 'JPEG')
-        save('library/_data__' + str(i), get_best())
+        Image.fromarray(get_best()[0]).save('/usr/local/share/jupyter/nbextensions/google.colab/strokes.jpg', 'JPEG')
+        save('library/_data__' + str(cfg.fitness[get_best()[1]]), get_best()[0])
 
 print('Total - %.6f seconds' % (time() - start), end='\t\t')
 print('Delta fitness -\t{}'.format(min(cfg.fitness) - init_min_fitness))
